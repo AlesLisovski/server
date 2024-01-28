2126ba38990a5113062f676f530c10f2340c83bf
Ales Lisovski <aleslisovskij@gmail.com>
2024-01-29 01:53:28 +0300

    improved code readability and fixed some bugs

--- main.cc
+++ main.cc
@@ -11,6 +11,7 @@
 #include <boost/ref.hpp>
 #include <csignal>
 #include <iostream>
+#include <map>
 #include <memory>
 #include <string>
 
@@ -26,24 +27,44 @@ public:
 
   asio::ip::tcp::socket &socket() { return socket_; }
 
-  void start(const std::string &received_message, Database &data) {
+  void start(const std::string &received_message, Database &data,
+             std::map<std::string, boost::asio::ip::tcp::socket *> &sockets,
+             const boost::system::error_code &error) {
     std::cout << "RECEIVED: " << received_message;
-
     MessageHandler handler(received_message, data);
 
     message_ = handler.GetResponse();
-    boost::asio::async_write(
-        socket_, boost::asio::buffer(message_),
-        boost::bind(&tcp_connection::handle_write, shared_from_this(),
-                    boost::asio::placeholders::error,
-                    boost::asio::placeholders::bytes_transferred));
+
+    if (handler.GetSocketStatus() ==
+        MessageHandler::SOCKET_STATUS::CREATE_SOCKET) {
+      sockets.insert(std::make_pair(handler.GetLogin(), &socket_));
+    }
+
+    if (handler.GetSocketStatus() ==
+        MessageHandler::SOCKET_STATUS::DELETE_SOCKET) {
+      sockets.erase(handler.GetLogin());
+    }
+
+    if (handler.GetSocketStatus() !=
+        MessageHandler::SOCKET_STATUS::DELETE_SOCKET) {
+      std::cout << "SHIPPED: " << message_ << std::endl;
+
+      boost::asio::async_write(
+          socket_, boost::asio::buffer(message_),
+          boost::bind(&tcp_connection::handle_write, shared_from_this(),
+                      boost::asio::placeholders::error,
+                      boost::asio::placeholders::bytes_transferred));
+    }
   }
 
 private:
   tcp_connection(boost::asio::io_service &io_service) : socket_(io_service) {}
 
-  void handle_write(const boost::system::error_code & /*error*/,
-                    size_t /*bytes_transferred*/) {}
+  void handle_write(const boost::system::error_code &error,
+                    size_t bytes_transferred) {
+    // if (error)
+    //
+  }
 
   asio::ip::tcp::socket socket_;
   std::string message_;
@@ -55,13 +76,13 @@ public:
       : acceptor_(io_service,
                   asio::ip::tcp::endpoint(
                       asio::ip::address_v4::from_string("127.0.0.1"), 8888)),
-        database_(DATABASE) {
+        database_(DB) {
     start_accept();
   }
 
   void CloseTcpServer() {
     std::cout << "\nClosing the server\n";
-    database_.SaveData();
+    database_.CloseDatabase();
   }
 
 private:
@@ -76,20 +97,25 @@ private:
 
   void handle_accept(tcp_connection::pointer new_connection,
                      const boost::system::error_code &error) {
-    strcpy(received_message_, "");
+    received_message_.clear();
+    received_message_.resize(4096);
+
     if (!error) {
       new_connection->socket().async_read_some(
-          boost::asio::buffer(received_message_, 4096),
-          boost::bind(&tcp_connection::start, new_connection, received_message_,
-                      boost::ref(database_)));
+          boost::asio::buffer(received_message_.data(),
+                              received_message_.size()),
+          boost::bind(&tcp_connection::start, new_connection,
+                      boost::ref(received_message_), boost::ref(database_),
+                      boost::ref(sockets_), boost::asio::placeholders::error));
     }
 
     start_accept();
   }
 
-  char received_message_[4096];
+  std::string received_message_;
   asio::ip::tcp::acceptor acceptor_;
   Database database_;
+  std::map<std::string, boost::asio::ip::tcp::socket *> sockets_;
 };
 
 tcp_server *server_ptr = nullptr;
--- message_handler/MessageHandler.h
+++ message_handler/MessageHandler.h
@@ -8,21 +8,34 @@
 class MessageHandler {
   Json::Value root_;
   std::string response_;
+  int socket_status_ = SOCKET_STATUS::PASSIVE_SOCKET;
+  std::string login_ = "";
 
 public:
   MessageHandler(const std::string &message, Database &database);
 
+  enum SOCKET_STATUS {
+    CREATE_SOCKET,
+    DELETE_SOCKET,
+    PASSIVE_SOCKET,
+  };
+
   std::string GetResponse() const;
+  std::string GetLogin() const;
   std::string RequestNotDefined() const;
-  std::string RequestRegistration(Database &database) const;
-  std::string RequestAuthorization(Database &database) const;
+  std::string RequestRegistration(Database &database);
+  std::string RequestAuthorization(Database &database);
   std::string RequestChangePassword(Database &database) const;
   std::string RequestDeleteAccout(Database &database) const;
   std::string RequestCleanDatabase(Database &database) const;
   std::string RequestMessage(Database &database) const;
-  std::string JsonToString(Json::Value &json) const;
+  std::string JsonToString(const Json::Value &json) const;
 
+  void RequestAppClosed();
+  void SetLogin(const std::string &login);
   void SetRequestStatus(Json::Value &json, const std::string &request,
                         const std::string &code,
                         const std::string &status) const;
+
+  int GetSocketStatus() const;
 };
